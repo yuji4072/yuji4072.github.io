d429346457389cb6b3e222b6ba2823d17813a5f9
syuji4072 <31642@toyota.kosen-ac.jp>
2021-02-01 17:44:53 +0900

    aaaaaaa

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-main>
+      <home></home>
+    </v-main>
+</template>
+
+<script>
+import Home from './views/Home.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    Home,
+  },
+
+  data: () => ({
+  }),
+};
+</script>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div v-bind:class = "type">
+        aaaaa
+    </div>
+    <div>
+      <button v-on:click = "changes">{{count}}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+
+    data: () => ({
+      type :"red",
+      count : 0
+    }),
+    methods:{
+      changes: function(){
+        if(this.type == "red")this.type = "blue"
+        else this.type = "red"
+        this.count += 1;
+      }
+    },
+  }
+</script>
+
+<style scoped>
+    .red{
+      color: red;
+    }
+    .blue{
+      color: blue;
+    }
+</style>
